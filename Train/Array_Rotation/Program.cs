2cdc787a4d23112ee9c7eee3ef2e7151f567912c
HristoHristovPanayotov <emailformyself123123@gmail.com>
2023-10-16 21:49:04 +0300

    domashno3

---
+++ Train/Array_Rotation/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+            int chislo = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < chislo; i++)
+            {
+                for (int j = arr.Length - 1; j >= 0; j--)
+                {
+                    int element = arr[arr.Length - 1];
+                    arr[arr.Length - 1] = arr[j];
+                    arr[j] = element;
+
+                }
+            }
+            Console.WriteLine(string.Join(" ", arr));
+
+        }
+    }
+}
---
+++ Train/Top_Integers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
