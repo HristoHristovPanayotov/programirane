9759e79d3438d701ea42d53d2a64e77d34dc6521
HristoHristovPanayotov <emailformyself123123@gmail.com>
2023-10-25 12:05:54 +0300

    methods

---
+++ methods/methods/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintNumberSign(number);
+        }
+        static void PrintNumberSign(int number)
+        {
+            
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+                
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else 
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+            
+                
+            
+        }
+    }
+
+}
